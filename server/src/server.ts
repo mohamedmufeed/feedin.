e979648da0a3997ecdadaa4781e63061525b5a65
mohamedmufeed <mohamedmufeed44@gmail.com>
2025-06-14 11:58:51 +0530

    fix:api bug-2

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: process.env.FRONT_END_URL_PROD,
+        origin: process.env.FRONT_END_URL,
         credentials: true,
     })
 );
